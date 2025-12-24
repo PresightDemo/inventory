6f4fc677645bfa42a8bead135ba99b4564ecae95
Vinay <vinay.panwar1604@gmail.com>
2025-12-24 14:50:08 +0800

    MethodLevelSecurityEnabled

--- src/main/java/com/presight/inventory/config/SecurityConfig.java
+++ src/main/java/com/presight/inventory/config/SecurityConfig.java
@@ -3,12 +3,13 @@ package com.presight.inventory.config;
 import com.presight.common_lib.security.web.BaseWebSecurityFilterChainFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-
+@EnableMethodSecurity
 public class SecurityConfig  {
 
     @Bean
